130f581dca832a763ba67944c0be5678bd98caa0
menth0s <marslite@pm.me>
2024-12-22 04:56:30 +0100

    Addressing 628

--- leetcode/628_maximum_product_of_three_numbers.py
+++ leetcode/628_maximum_product_of_three_numbers.py
@@ -1,9 +1,9 @@
 """628. Maximum Product of Three Numbers"""
-#loom: https://www.loom.com/share/b7ce6f18baac4ddfa9cfae3fe37aed0c
 class Solution:
     def maximumProduct(self, nums: list[int]) -> int:
         """
         #Finds the maximum product of three numbers in the given list.
+        #loom: https://www.loom.com/share/b7ce6f18baac4ddfa9cfae3fe37aed0c
         """
         #Time Complexity: O(n log n)
         #Space Complexity: O(1)
@@ -11,7 +11,7 @@ class Solution:
         nums = sorted(nums)
         # Step 2: Consider two possible options to calculate the maximum product:
         # Option 1: Use the last three elements of the sorted array.
-        # This works because if the array contains only positive numbers, 
+        # This works because if the array contains only positive numbers,
         # the three largest numbers will yield the maximum product.
         # Specifically: nums[-1] * nums[-2] * nums[-3].
         # Option 2: Use the first two elements and the last element.
@@ -23,4 +23,6 @@ class Solution:
 
         #Given these two possible options for both all positive/negative integers and mixed positive
         #and negative integers we will return the one which yields the largest product of three numbers.
-        return max( nums[-1] * nums[-2] * nums[-3], nums[0] * nums[1] * nums[len(nums)-1])
+        return max( nums[-1] * nums[-2] * nums[-3],
+                    nums[0] * nums[1] * nums[len(nums)-1])
+
