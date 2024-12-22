6a396c78c81fcfc76fd653e45a0a1950af783f13
menth0s <marslite@pm.me>
2024-12-22 04:43:11 +0100

    Addressing 57

--- leetcode/57_insert_interval.py
+++ leetcode/57_insert_interval.py
@@ -5,13 +5,14 @@ class Solution:
         # This function inserts a new interval into a list of sorted, non-overlapping intervals
         #and merges any overlapping intervals to ensure that the resulting intervals remain
         #sorted and non-overlapping.
-        #Loom: https://www.loom.com/share/49b3a7e8faf14012b274297443082570?sid=febff73b-da83-4333-b3e6-32789ee123a3
+        #Loom: https://www.loom.com/share/
+        #49b3a7e8faf14012b274297443082570?sid=febff73b-da83-4333-b3e6-32789ee123a3
         """
         #We will start by declaring an empty array that we will use to store the result
         result = []
         #Time Complexity: O(n)
         #Space Complexity: O(n)
-        #We want to iterate over the range of intervals and check whether 
+        #We want to iterate over the range of intervals and check whether
         #First I'll be checkign that newInterval[1] is smaller than the start of my first interval
         for i in range(len(intervals)):
             if newInterval[1] < intervals[i][0]:
@@ -25,4 +26,5 @@ class Solution:
                 newInterval = (min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1]))
         result.append(newInterval)
         return result
+
 
