653055b092ab8edeb8800ba867532a9dd7012fc9
menth0s <marslite@pm.me>
2024-12-22 05:01:43 +0100

    Addressing 75

--- leetcode/75_sort_colors.py
+++ leetcode/75_sort_colors.py
@@ -2,9 +2,12 @@
 class Solution:
     def sortColors(self, nums: list[int]) -> None:
         """
-        #This function implements the Dutch National Flag problem using Bucket Sort to sort an array containing 
-        #three distinct values: 0, 1, and 2. It first counts the occurrences of each value in a fixed-size array 
-        #(bucket). Then, it overwrites the original array with the sorted values based on the counts.
+        #This function implements the Dutch National Flag problem using Bucket Sort 
+        # to sort an array containing 
+        #three distinct values: 0, 1, and 2. It first counts the occurrences of each value 
+        #in a fixed-size array 
+        #(bucket). Then, it overwrites the original array with 
+        # the sorted values based on the counts.
 
         #Loom: https://www.loom.com/share/ce2e6b5bdc4942d5b97c5614732741cc
         """
@@ -18,8 +21,8 @@ class Solution:
         # Since we only have 3 possible colors (0, 1, 2), we create an array of size 3.
         count = [0,0,0]
         #Step 2: We now fill the buckets with their respective colors
-        # We iterate through the nums array to count the colors 
-        for color in nums:  
+        # We iterate through the nums array to count the colors
+        for color in nums:
             count[color] +=1
 
         # We will now iterate through the `count` array to overwrite nums.
@@ -29,3 +32,4 @@ class Solution:
             for _ in range(count[i]):
                 nums[index] = i
                 index +=1
+
